0238d85d6cfd6c3cdfafe8ff9a4d22a0272802d0
Akhilesh verma <Akhileshverma207@gmail.com>
2026-01-21 23:50:05 +0530

    program palandrom

---
+++ palandrome.py
@@ -0,0 +1,38 @@
+# find the palandrome number
+
+# i = 1
+# n = int(input("enter any number : ")) #5268
+# sum = 0
+# rev = 0
+# m = n
+# result = ""
+# while n>0:
+
+#     r = n % 10
+#     n = n // 10
+#     rev = rev *10 +r
+#     i += 1
+    
+#     if(rev==m):
+#         result = "it is a palandrome"
+#     else:
+#         result = "it is not a palandrome"
+
+# print(result)
+
+# _____________________________________________________________________
+
+# find the sum of (5268) number
+# input = 5268  output = 21
+
+i = 1
+n = int(input("enter any number : "))
+sum = 0
+
+
+while n > 0:
+    r = n % 10
+    sum += r
+    n = n // 10
+    i += 1
+print(sum)
