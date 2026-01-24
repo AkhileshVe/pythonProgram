282c6eb12d58c34e3603efd8d7d0bd2c8d7fd938
Akhilesh verma <Akhileshverma207@gmail.com>
2026-01-24 23:11:21 +0530

    nested logical program

--- nestedLoop.py
+++ nestedLoop.py
@@ -12,8 +12,19 @@
 #     i +=1
 
 
+# for i in range(1, 6):
+#     for j in range(1,6):
+#         if(j <= i):
+#             print(j,end = "")
+
+#         else:
+#             print(" ", end = " ")
+
+
 for i in range(1, 6):
-    for j in range(1,6):
-        if(j <= i):
-            print(j, end = " ")
-       
+    for j in range(1, 6):
+        if j <= i:
+            print(j, end=" ")
+        else:
+            print(" ", end=" ")
+    print()   # new line after each row
