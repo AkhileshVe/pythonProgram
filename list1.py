bfb0e20c9b896bddbcd36c9f54cdbf97f222a755
Akhilesh verma <Akhileshverma207@gmail.com>
2026-01-30 22:59:00 +0530

    collection

--- list1.py
+++ list1.py
@@ -1,6 +1,31 @@
 ls =[] # empty list
 
-print(ls) # []
-print(type(ls)) # <class list>
-print(id(ls))  # address of memory 4349360512
-print(ls.__sizeof__()) # default size is 40
+# print(ls) # []
+# print(type(ls)) # <class list>
+# print(id(ls))  # address of memory 4349360512
+# print(ls.__sizeof__()) # default size is 40
+
+ls1 = [76, 34, 89, 45, 24, 66, 59]
+print(ls1*3)
+print(ls1) # print the list 
+print(len(ls1)) # find the length of list
+print(min(ls1)) # find the minimum value of list 
+print(max(ls1)) # find the maximum value of list
+print(sum(ls1)) # find the sum of all values of list
+
+# print(ls1[2]) # 89 this is how we can find the index
+# print 
+
+# i = 0
+# for x in ls1:
+#     ls1[i] = x +2
+#     i += 1
+
+# print(ls1)
+
+
+
+# for x in ls1:
+#     if(x%2==0):
+#         print(x)
+   
