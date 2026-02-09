894de9dc677232d3e5b4af0020c7c788f3153937
Akhilesh verma <Akhileshverma207@gmail.com>
2026-02-09 23:22:20 +0530

    prime by function

---
+++ Function/calculator.py
@@ -0,0 +1,53 @@
+def message(mess):
+    print("\t","----------------------------------------------------")
+    print("\t", "\t",        mess)
+    print("\t","----------------------------------------------------")
+
+
+def calculator():
+    message("Welcome to calculator")
+    num1 = int(input("Enter first number : "))
+    num2 = int(input("Enter second number : "))
+
+    loop = True
+
+
+    while loop:
+        print("Press 'A' for addition")
+        print("Press 'S' for substraction")
+        print("Press 'D' for division")
+        print("Press 'M' for multiplication")
+        print("Press 'X' for exit")
+
+        choise = input("Enter anyone of them : ")
+
+        if(choise == "A"):
+            print("\t","----------------------------------------------------")
+            r = num1 + num2
+            print("\t", "\t"    ,"Your addition of", num1, "+",num2, "=", r)
+            print("\t","----------------------------------------------------")
+        
+        elif(choise == "S"):
+            print("\t","----------------------------------------------------")
+            r = num1 - num2
+            print("\t", "\t"    ,"Your substraction of", num1, "-",num2, "=", r)
+            print("\t","----------------------------------------------------")
+
+        elif(choise == "M"):
+            print("\t","----------------------------------------------------")
+            r = num1 * num2
+            print("\t", "\t"    ,"Your multiplication of", num1, "*",num2, "=", r)
+            print("\t","----------------------------------------------------")
+
+        elif(choise == "D"):
+            r = num1 / num2
+            print("\t","----------------------------------------------------")
+            print("\t", "\t"    ,"\t", "\t","Your division of", num1, "/",num2, "=", r)
+            print("\t","----------------------------------------------------")
+
+        elif(choise == "X","E","x"):
+            loop = False
+
+    message("App calculator se bahar aa gaye")
+
+calculator()
--- functions.py
+++ functions.py
@@ -1,13 +1,93 @@
-def display():
-    print("hello display")
+# def display():
+#     print("hello display")
 
-# display()
+# # display()
 
 
-def addition(a,b,c):
-    add = a + b
-    print(add)
-    c()
+# def addition(a,b,c):
+#     add = a + b
+#     print(add)
+#     c()
     
 
-addition(4,5, display)
+# addition(4,5, display)
+
+# def add(a, b):
+#      print( a + b)
+
+# add(7, 3)
+
+
+# with return , without argument
+
+# def factorial():
+#     n = int(input("Enter any number for factorial : "))
+#     i = 1
+#     fact = 1
+
+#     while i <= n:
+#         fact *= i 
+#         i+=1
+#     return fact
+
+
+# result = factorial()
+# print("fact",result+5)
+
+
+# with return , without argument
+ 
+# def factorial():
+#     n = int(input("Enter any number for factorial : "))
+#     i = 1
+#     fact = 1
+
+#     while i <= n:
+#         fact *= i 
+#         i+=1
+#     print( fact)
+
+
+# factorial()
+
+
+
+# find the four greatest number
+# No return type with argument
+
+# def greatest(a,b,c,d):
+#     if(a>b and a>c and a>d):
+#         print(a ," is grratest")
+#     elif(b>a and b>c and b>d):
+#         print(b ," is grratest")
+#     elif(c>b and c>a and c>d):
+#         print(c ," is grratest")
+#     elif(d>b and d>c and d>a):
+#         print(d ," is grratest")
+
+
+# a=int(input("Enter first number : "))
+# b=int(input("Enter second number : "))
+# c=int(input("Enter third number : "))
+# d= int(input("Enter forth number : "))
+
+# greatest(a,b,c,d)
+
+
+def check_Primre(x):
+    i = 2
+    flag =1
+    while i < x and flag == 1:
+        if(x % i==0):
+            flag = 0
+        i += 1
+    return flag 
+
+n = int(input("Enter a number : "))
+
+res = check_Primre(n)
+
+if(res == 1):
+    print("Prime number.......")
+else:
+    print("Not prime number ......")
