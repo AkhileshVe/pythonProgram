3fddf1c0f7348790fada3efa4f6545a08ad7ab41
Akhilesh verma <Akhileshverma207@gmail.com>
2026-02-20 23:27:47 +0530

    file handling

--- prime.py
+++ prime.py
@@ -1,72 +1,92 @@
-i = 2
-n = int(input("Enter a number: "))
-count = 0
-is_prime = True
+# i = 2
+# n = int(input("Enter a number: "))
+# count = 0
+# is_prime = True
 
-if n <= 1:
+# if n <= 1:
 
-    is_prime = False
-else:
-    while i < n:
-        if n % i == 0:
-            count+=1
-            is_prime = False
-            break
-        i += 1
+#     is_prime = False
+# else:
+#     while i < n:
+#         if n % i == 0:
+#             count+=1
+#             is_prime = False
+#             break
+#         i += 1
 
-if count == 0:
-    print(n, "is a prime number")
-else:
-    print(n, "is not a prime number")
+# if count == 0:
+#     print(n, "is a prime number")
+# else:
+#     print(n, "is not a prime number")
 
 
 
-# prime number using for loop
+# # prime number using for loop
 
-n = int(input("Enter a number: "))
-count = 0
+# n = int(input("Enter a number: "))
+# count = 0
 
-for x in  range(2,n):
-    if(n % x == 0):
-        count += 1
-        break
+# for x in  range(2,n):
+#     if(n % x == 0):
+#         count += 1
+#         break
     
 
-if(count == 0):
-    print(n, "is a prime number")
-else:
-    print(n, "is not a prime number")
+# if(count == 0):
+#     print(n, "is a prime number")
+# else:
+#     print(n, "is not a prime number")
 
 
 
-# ++++++++++++++++++++++++++++++++++++...4...+++++++++++++++++++++++++++
+# # ++++++++++++++++++++++++++++++++++++...4...+++++++++++++++++++++++++++
 
-# when i enter 10 the output is (2 3 5 7 11 13 17 19 23 29)
+# # when i enter 10 the output is (2 3 5 7 11 13 17 19 23 29)
 
-n = int(input("enter any value : "))
-i = 2
-count = 0
+# n = int(input("enter any value : "))
+# i = 2
+# count = 0
 
-while count < n:
-    j=2
-    while j<i:
-        if( i % j == 0):
-            break
-        j += 1
-    if(j == i):
-        print(i, end=" ")
-        count +=1
+# while count < n:
+#     j=2
+#     while j<i:
+#         if( i % j == 0):
+#             break
+#         j += 1
+#     if(j == i):
+#         print(i, end=" ")
+#         count +=1
 
-    i +=1
+#     i +=1
 
 
-#  prime number up to n using for loop 
+# #  prime number up to n using for loop 
 
 n = int(input("Enter any number: "))
 
-for x in range(2, n):
-    for j in range(2, x):
-        if x % j == 0:
+# for x in range(2, n):
+#     for j in range(2, x):
+#         if x % j == 0:
+#             break
+#     else:
+#         print(x)
+
+# for  x in range(2, n ):
+#     for j in range(2,x):
+#         if (x % j == 0):
+#             break
+#     else:
+#         print(x)
+
+count = 0
+i = 2
+while count <  n :
+    j = 2
+    while j < i:
+        if(i % j == 0):
             break
-    else:
-        print(x)
+        j += 1
+    if(j == i):
+        count += 1
+        print(i)
+    i += 1
