5cc8edf27d4701b906debd06ce2e65557c6036f5
Akhilesh verma <Akhileshverma207@gmail.com>
2026-04-11 23:53:27 +0530

    learn pandas

--- pandas_program.py/p4.py
+++ pandas_program.py/p4.py
@@ -29,3 +29,10 @@ print("tex_total.  : ",tex_total)
 print("da_total.  : ",da_total)
 print("ta_total.  : ",ta_total)
 print("salary_total.  : ",salary_total)
+
+
+
+print("tex_total.  : ",tex_total)
+print("da_total.  : ",da_total)
+print("ta_total.  : ",ta_total)
+print("salary_total.  : ",salary_total)
